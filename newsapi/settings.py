d679b4a53cee94f490a9539b55e2a2bf40215f6c
Hikmet Mirzeyev <124464293+MageHiko@users.noreply.github.com>
2025-10-10 03:12:31 +0000

    commit

--- newsapi/settings.py
+++ newsapi/settings.py
@@ -34,8 +34,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleaware',
-    'django.middleware.common.CommonMiddleware.'
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
